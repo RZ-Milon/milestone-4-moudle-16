191226c846ab36b531a18f9a6e25c99f8b84d70d
RZMilon <rzmilon@gmail.com>
2022-07-31 22:01:13 +0600

    practice problem solving done

---
+++ practice-problem.js
@@ -0,0 +1,30 @@
+// Problem 1
+var harryMomGave = 1000;
+var orangePrice = 300;
+var applePrice = 400;
+var totalCost = orangePrice + applePrice;
+var cashReturn = harryMomGave - totalCost;
+console.log(cashReturn);
+
+// Problem 2
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.5;
+var quantity = 5;
+var totalMarks = math + biology + chemistry + physics + bangla;
+var average = totalMarks / quantity ;
+average = average.toFixed(2);
+console.log(average);
+
+// Problem 3
+var firstSentence = "I am going to be";
+var lastSentence = "an awesome web developer";
+var fullSentence = firstSentence + '' + lastSentence;
+console.log(fullSentence);
+
+// Problem 4
+var number = 119;
+var remainder = 119 % 5;
+console.log(remainder);
---
+++ remainder.js
@@ -0,0 +1,10 @@
+var mangoes = 7;
+var person = 2;
+var remainder = mangoes % person
+
+// / this means vagfol
+// % this means vagshes
+
+console.log(mangoes/person);
+console.log(remainder);
+
